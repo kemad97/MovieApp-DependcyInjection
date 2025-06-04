06aa01744cff515773707b7d5f4f9d15fc6c40bb
kemad97 <90790284+kemad97@users.noreply.github.com>
2025-06-04 12:54:20 +0300

    Initial Commit

---
+++ MovieApp DependcyInjectionTests/MovieApp_DependcyInjectionTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp_DependcyInjectionTests.swift
+//  MovieApp DependcyInjectionTests
+//
+//  Created by Kerolos on 04/06/2025.
+//
+
+import Testing
+@testable import MovieApp_DependcyInjection
+
+struct MovieApp_DependcyInjectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
